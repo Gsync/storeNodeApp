ee671493c2af88fd94b30586623317f5fbeef392
Khuram Niaz <knshaikh@gmail.com>
2017-12-21 17:12:44 -0700

    add favorite heart float animation (heart.scss)

--- public/javascripts/modules/heart.js
+++ public/javascripts/modules/heart.js
@@ -8,7 +8,12 @@ function ajaxHeart(e) {
         .then(res => {
             const isHearted = this.heart.classList.toggle('heart__button--hearted');
             $('.heart-count').textContent = res.data.hearts.length; //update heart count
-            console.log(isHearted);
+            if (isHearted) {
+                this.heart.classList.add('heart__button--float'); //if hearted add the float class for css animation
+                setTimeout(() => {
+                    this.heart.classList.remove('heart__button--float');
+                }, 2500);
+            };
         })
         .catch(console.error);
 }
